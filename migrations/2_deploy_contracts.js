87bf0da478c781a82bf81f5694b9a3f89f4c33d9
Vinay <vinay.calastry@gmail.com>
2019-04-25 17:06:11 -0700

    Testing Pure Eth solution

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,8 @@
 var SensorContract = artifacts.require("./SensorContract.sol");
+var EthSensorContract = artifacts.require("./EthSensorContract.sol");
 
 module.exports = function(deployer){
 
     deployer.deploy(SensorContract);
-
+    deployer.deploy(EthSensorContract);
 };
